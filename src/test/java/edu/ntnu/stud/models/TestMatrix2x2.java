70de00b9e59c8f2dd58ae11afba8a98eb3a71d5e
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-02-23 22:54:31 +0100

    test(TestMatrix2x2): add tests and javadoc for the Matrix2x2 record

--- src/test/java/edu/ntnu/stud/models/TestMatrix2x2.java
+++ src/test/java/edu/ntnu/stud/models/TestMatrix2x2.java
@@ -4,52 +4,70 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+/**
+ * This class is used to test the functionality of the Matrix2x2 class.
+ * It includes tests for the constructor, and the multiply method.
+ * Each test method in this class is annotated with the @Test annotation.
+ * The setup method, annotated with @Before, is used to initialize the Matrix2x2 object and Vector2D object used in the tests.
+ * The precisionDelta variable is used to compare double values for equality.
+ */
 public class TestMatrix2x2 {
 
     private Matrix2x2 matrix;
     private Vector2D vec1;
     private final double precisionDelta = 0.0001;
 
+    /**
+     * This method is executed before each test.
+     * It initializes a Matrix2x2 object with specific values and a Vector2D object with specific coordinates.
+     */
     @Before
     public void setup(){
         this.matrix = new Matrix2x2(1, 3, 2, 4);
         this.vec1 = new Vector2D(2, 3);
     }
 
+    /**
+     * This test checks if the constructor of the Matrix2x2 class works correctly.
+     */
     @Test
     public void constructor_constructorIsCorrect_true(){
-        assertEquals(1, matrix.getA00(), this.precisionDelta);
-        assertEquals(3, matrix.getA01(), this.precisionDelta);
-        assertEquals(2, matrix.getA10(), this.precisionDelta);
-        assertEquals(4, matrix.getA11(), this.precisionDelta);
+        assertEquals(1, matrix.a00(), this.precisionDelta);
+        assertEquals(3, matrix.a01(), this.precisionDelta);
+        assertEquals(2, matrix.a10(), this.precisionDelta);
+        assertEquals(4, matrix.a11(), this.precisionDelta);
     }
 
-    @Test
-    public void getA00_A00is1_true(){
-        assertEquals(1, matrix.getA00(), this.precisionDelta);
-    }
-
-    @Test
-    public void getA01_A01is3_true(){ assertEquals(3, matrix.getA01(), this.precisionDelta);
-    }
-
-    @Test
-    public void getA10_A10is2_true(){
-        assertEquals(2, matrix.getA10(), this.precisionDelta);
-    }
-
-    @Test
-    public void getA11_A11is4_true(){
-        assertEquals(4, matrix.getA11(), this.precisionDelta);
-    }
-
+    /**
+     * This test checks if the multiply method of the Matrix2x2 class works correctly.
+     */
     @Test
     public void multiply_multiplicationIsCorrect_true(){
         Vector2D vec2 = matrix.multiply(vec1);
 
-        assertEquals(matrix.getA00() * vec1.getX0() + matrix.getA01() * vec1.getX1(), vec2.getX0(), this.precisionDelta);
-        assertEquals(matrix.getA10() * vec1.getX0() + matrix.getA11() * vec1.getX1(), vec2.getX1(), this.precisionDelta);
+        assertEquals(matrix.a00() * vec1.getX0() + matrix.a01() * vec1.getX1(), vec2.getX0(), this.precisionDelta);
+        assertEquals(matrix.a10() * vec1.getX0() + matrix.a11() * vec1.getX1(), vec2.getX1(), this.precisionDelta);
     }
 
+    /**
+     * This test checks if the multiply method of the Matrix2x2 class throws a NullPointerException when multiplying with a null vector.
+     */
+    @Test(expected = NullPointerException.class)
+    public void multiply_multiplicationWithNull_throws(){
+        matrix.multiply(null);
+    }
+
+    /**
+     * This test checks if the multiply method of the Matrix2x2 class correctly handles the multiplication of a matrix with a vector with extreme values.
+     */
+    @Test
+    public void multiply_multiplicationWithExtremeValues_isInfinite(){
+        Vector2D vec2 = new Vector2D(Double.MAX_VALUE, Double.MAX_VALUE);
+        Vector2D vec3 = matrix.multiply(vec2);
+
+        assertTrue(Double.isInfinite(vec3.getX0()));
+        assertTrue(Double.isInfinite(vec3.getX1()));
+    }
 }
