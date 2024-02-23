bf24600adb1f137439e97d6e430a494646ee7f85
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-02-23 22:42:25 +0100

    docs(Matrix2x2): add new javadoc for record

--- src/main/java/edu/ntnu/stud/models/Matrix2x2.java
+++ src/main/java/edu/ntnu/stud/models/Matrix2x2.java
@@ -1,6 +1,18 @@
 package edu.ntnu.stud.models;
 
+/**
+ * Represents a 2x2 matrix.
+ * This class is a record, which is a special kind of class in Java.
+ * Good for representing immutable data classes.
+ * The Matrix2x2 record has four fields representing the elements of the matrix.
+ */
 public record Matrix2x2(double a00, double a01, double a10, double a11) {
+    /**
+     * Multiplies this matrix with a given vector.
+     *
+     * @param vec the vector to multiply with
+     * @return a new vector that is the result of the multiplication
+     */
     public Vector2D multiply(Vector2D vec) {
         return new Vector2D(this.a00 * vec.getX0() + this.a01 * vec.getX1(),
             this.a10 * vec.getX0() + this.a11 * vec.getX1());
