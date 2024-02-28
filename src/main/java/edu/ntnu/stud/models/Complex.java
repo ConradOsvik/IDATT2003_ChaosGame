cf11df9a77e8f93652d460c99d4aa2bb2fa5ffb7
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-02-28 01:11:15 +0100

    docs(models): add missing javadocs

--- src/main/java/edu/ntnu/stud/models/Complex.java
+++ src/main/java/edu/ntnu/stud/models/Complex.java
@@ -1,54 +1,71 @@
 package edu.ntnu.stud.models;
 
 /**
- * Represents a complex number with a real and an imaginary part.
- * Extends the Vector2D class.
+ * Represents a complex number with a real and an imaginary part. Extends the Vector2D class.
  */
 public class Complex extends Vector2D {
 
-    /**
-     * Constructs a new Complex object.
-     *
-     * @param realPart The real part of the complex number.
-     * @param imaginaryPart The imaginary part of the complex number.
-     */
-    public Complex(double realPart, double imaginaryPart) {
-       super(realPart, imaginaryPart);
-    }
+  /**
+   * Constructs a new Complex object.
+   *
+   * @param realPart      The real part of the complex number.
+   * @param imaginaryPart The imaginary part of the complex number.
+   */
+  public Complex(double realPart, double imaginaryPart) {
+    super(realPart, imaginaryPart);
+  }
 
-    /**
-     * Returns the real part of the complex number.
-     *
-     * @return The real part of the complex number.
-     */
-    public double getReal() {
-        return getX0();
-    }
+  /**
+   * Returns the real part of the complex number.
+   *
+   * @return The real part of the complex number.
+   */
+  public double getReal() {
+    return getX0();
+  }
 
-    /**
-     * Returns the imaginary part of the complex number.
-     *
-     * @return The imaginary part of the complex number.
-     */
-    public double getImaginary() {
-        return getX1();
-    }
+  /**
+   * Returns the imaginary part of the complex number.
+   *
+   * @return The imaginary part of the complex number.
+   */
+  public double getImaginary() {
+    return getX1();
+  }
+
+  /**
+   * Adds the given vector to this complex number.
+   *
+   * @param vec The vector to be added to this complex number.
+   * @return A new Complex object that represents the sum of this complex number and the given
+   * vector.
+   */
   @Override
   public Complex add(Vector2D vec) {
     return new Complex(this.getReal() + vec.getX0(), this.getImaginary() + vec.getX1());
   }
 
-    /**
-     * Returns the square root of the complex number.
-     *
-     * @return A new Complex object that represents the square root of the complex number.
-     */
-    public Complex sqrt(){
-        double r = Math.hypot(this.getReal(), this.getImaginary());
-        double realPart = Math.sqrt((r + this.getReal()) / 2);
-        double imaginaryPart = Math.signum(this.getImaginary()) * Math.sqrt((r - this.getReal()) / 2);
-        return new Complex(realPart, imaginaryPart);
-    }
+  /**
+   * Returns the square root of the complex number.
+   *
+   * @return A new Complex object that represents the square root of the complex number.
+   */
+  public Complex sqrt() {
+    double r = Math.hypot(this.getReal(), this.getImaginary());
+    double realPart = Math.sqrt((r + this.getReal()) / 2);
+    double imaginaryPart = Math.signum(this.getImaginary()) * Math.sqrt((r - this.getReal()) / 2);
+    return new Complex(realPart, imaginaryPart);
+  }
+
+  /**
+   * Checks if the complex number is equal to the specified object. The result is true if and only
+   * if the argument is not null and is a Complex object that has the same real and imaginary parts
+   * as this Complex object.
+   *
+   * @param obj The object to compare this Complex against
+   * @return true if the given object represents a Complex equivalent to this complex number, false
+   * otherwise
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
@@ -62,13 +79,14 @@ public class Complex extends Vector2D {
         Double.compare(complex.getImaginary(), getImaginary()) == 0;
   }
 
-    /**
-     * Returns a string representation of the complex number.
-     *
-     * @return A string representation of the complex number in the format "real + imaginary".
-     */
-    @Override
-    public String toString() {
-        return this.getReal() + (this.getImaginary() < 0 ? " - " : " + ") + Math.abs(this.getImaginary()) + "i";
-    }
+  /**
+   * Returns a string representation of the complex number.
+   *
+   * @return A string representation of the complex number in the format "real + imaginary".
+   */
+  @Override
+  public String toString() {
+    return this.getReal() + (this.getImaginary() < 0 ? " - " : " + ") + Math.abs(
+        this.getImaginary()) + "i";
+  }
 }
--- src/main/java/edu/ntnu/stud/models/Matrix2x2.java
+++ src/main/java/edu/ntnu/stud/models/Matrix2x2.java
@@ -1,23 +1,31 @@
 package edu.ntnu.stud.models;
 
 /**
- * Represents a 2x2 matrix.
- * This class is a record, which is a special kind of class in Java.
- * Good for representing immutable data classes.
- * The Matrix2x2 record has four fields representing the elements of the matrix.
+ * Represents a 2x2 matrix. This class is a record, which is a special kind of class in Java. Good
+ * for representing immutable data classes. The Matrix2x2 record has four fields representing the
+ * elements of the matrix.
  */
 public record Matrix2x2(double a00, double a01, double a10, double a11) {
-    /**
-     * Multiplies this matrix with a given vector.
-     *
-     * @param vec the vector to multiply with
-     * @return a new vector that is the result of the multiplication
-     */
-    public Vector2D multiply(Vector2D vec) {
-        return new Vector2D(this.a00 * vec.getX0() + this.a01 * vec.getX1(),
-            this.a10 * vec.getX0() + this.a11 * vec.getX1());
-    }
 
+  /**
+   * Multiplies this matrix with a given vector.
+   *
+   * @param vec the vector to multiply with
+   * @return a new vector that is the result of the multiplication
+   */
+  public Vector2D multiply(Vector2D vec) {
+    return new Vector2D(this.a00 * vec.getX0() + this.a01 * vec.getX1(),
+        this.a10 * vec.getX0() + this.a11 * vec.getX1());
+  }
+
+  /**
+   * Checks if this matrix is equal to the specified object. The result is true if and only if the
+   * argument is not null and is a Matrix2x2 object that has the same elements as this matrix.
+   *
+   * @param obj The object to compare this Matrix2x2 against
+   * @return true if the given object represents a Matrix2x2 equivalent to this matrix, false
+   * otherwise
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
@@ -33,6 +41,11 @@ public record Matrix2x2(double a00, double a01, double a10, double a11) {
         Double.compare(matrix.a11, a11) == 0;
   }
 
+  /**
+   * Returns a string representation of the matrix.
+   *
+   * @return A string representation of the matrix in the format "[[a00, a01], [a10, a11]]".
+   */
   @Override
   public String toString() {
     return "[[" + a00 + ", " + a01 + "], [" + a10 + ", " + a11 + "]]";
