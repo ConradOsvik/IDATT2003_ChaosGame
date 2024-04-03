3620a0fa90c7d2f205f57ad42e8edfdde89790d5
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-04-04 00:57:04 +0200

    feat: add basic cli command group

---
+++ src/main/java/edu/ntnu/stud/commands/CLICommandGroup.java
@@ -0,0 +1,17 @@
+package edu.ntnu.stud.commands;
+
+import edu.ntnu.stud.controllers.CLIController;
+import edu.ntnu.stud.input.ValidatedInput;
+import edu.ntnu.stud.views.CLI;
+
+public class CLICommandGroup extends CommandGroup{
+  public CLICommandGroup(CLIController controller, CLI cli, ValidatedInput validatedInput){
+    super("Select a command:");
+
+    this.addCommand(new ReadFileCommand(controller, cli, validatedInput));
+    this.addCommand(new WriteToFileCommand(cli, validatedInput));
+    this.addCommand(new RunStepsCommand(controller, cli, validatedInput));
+    this.addCommand(new PrintFractalCommand(controller));
+    this.addCommand(new ExitCommand(cli));
+  }
+}
