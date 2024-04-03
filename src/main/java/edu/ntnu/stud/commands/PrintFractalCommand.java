74eb41e2701fa6edc73fbfe8745c20428e9a2f5e
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-04-04 01:05:57 +0200

    feat: create command for printing a fractal

---
+++ src/main/java/edu/ntnu/stud/commands/PrintFractalCommand.java
@@ -0,0 +1,25 @@
+package edu.ntnu.stud.commands;
+
+import edu.ntnu.stud.controllers.CLIController;
+
+public class PrintFractalCommand implements Command {
+  private final CLIController controller;
+  public PrintFractalCommand(CLIController controller) {
+    this.controller = controller;
+  }
+  @Override
+  public String getName() {
+    return "Print fractal";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Prints the fractal to the console";
+  }
+
+  @Override
+  public void execute() {
+    System.out.println("Executing command: " + getName());
+    controller.printFractal();
+  }
+}
