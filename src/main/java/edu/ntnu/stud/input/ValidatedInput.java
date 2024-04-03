af3b29f779f8e4e2b8ae152054f034d72fb23244
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-04-03 18:27:57 +0200

    feat: add validated input

---
+++ src/main/java/edu/ntnu/stud/input/ValidatedInput.java
@@ -0,0 +1,40 @@
+package edu.ntnu.stud.input;
+
+import edu.ntnu.stud.exceptions.InvalidFormatException;
+import edu.ntnu.stud.exceptions.InvalidInputException;
+import edu.ntnu.stud.views.CLI;
+
+public class ValidatedInput {
+  private final CLI cli;
+  private final InputHandler inputHandler;
+  public ValidatedInput(CLI cli){
+    this.cli = cli;
+    this.inputHandler = new InputHandler();
+  }
+  public <T> T execute(ThrowingSupplier<T> supplier, String prompt){
+    while(true){
+      cli.displayMessage(prompt);
+      try{
+        return supplier.get();
+      } catch (InvalidFormatException | InvalidInputException e){
+        cli.displayErrorMessage(e.getMessage());
+      }
+    }
+  }
+
+  public String getString(String prompt){
+    return execute(inputHandler::getString, prompt);
+  }
+
+  public int getInt(String prompt){
+    return execute(inputHandler::getInt, prompt);
+  }
+
+  public double getDouble(String prompt){
+    return execute(inputHandler::getDouble, prompt);
+  }
+
+  public double getSign(String prompt){
+    return execute(inputHandler::getSign, prompt);
+  }
+}
