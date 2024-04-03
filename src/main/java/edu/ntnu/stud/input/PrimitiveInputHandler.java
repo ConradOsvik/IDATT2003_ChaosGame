42129286dd7a02a3e37524a99a6566b2d9452c6f
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-04-03 18:26:51 +0200

    feat: add primitive input handler

---
+++ src/main/java/edu/ntnu/stud/input/PrimitiveInputHandler.java
@@ -0,0 +1,53 @@
+package edu.ntnu.stud.input;
+
+import edu.ntnu.stud.exceptions.InvalidFormatException;
+import edu.ntnu.stud.exceptions.InvalidInputException;
+import java.util.Scanner;
+
+class PrimitiveInputHandler {
+  private final Scanner scanner;
+
+  public PrimitiveInputHandler(){
+    this.scanner = new Scanner(System.in);
+  }
+
+  private <T> T wrap(ThrowingSupplier<T> supplier) throws InvalidFormatException, InvalidInputException{
+    try{
+      return supplier.get();
+    } catch (InvalidInputException e){
+      throw new InvalidInputException(e.getMessage());
+    } catch (Exception e){
+      throw new InvalidFormatException();
+    }
+  }
+
+  private void isEmpty(String input) throws InvalidInputException{
+    if(input.isEmpty()){
+      throw new InvalidInputException("Input cannot be empty, please try again.");
+    }
+  }
+
+  public String getString() throws InvalidFormatException, InvalidInputException {
+    return wrap(() -> {
+      String input = scanner.nextLine();
+      isEmpty(input);
+      return input;
+    });
+  }
+
+  public int getInt() throws InvalidFormatException, InvalidInputException {
+    return wrap(() -> {
+      String input = scanner.nextLine();
+      isEmpty(input);
+      return Integer.parseInt(input);
+    });
+  }
+
+  public double getDouble() throws InvalidFormatException, InvalidInputException {
+    return wrap(() -> {
+      String input = scanner.nextLine();
+      isEmpty(input);
+      return Double.parseDouble(input);
+    });
+  }
+}
