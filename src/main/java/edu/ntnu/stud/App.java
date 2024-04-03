5e6571ed946f84d957fe9ca2925163a991c3b326
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-04-04 01:08:22 +0200

    feat: add support for cli menu in application

--- src/main/java/edu/ntnu/stud/App.java
+++ src/main/java/edu/ntnu/stud/App.java
@@ -1,9 +1,22 @@
 package edu.ntnu.stud;
 
-/** Hello world! */
-public class App {
+import edu.ntnu.stud.controllers.CLIController;
+import edu.ntnu.stud.views.CLI;
 
+public class App {
+  private static CLIController cliController;
+  public static void init(){
+    CLI cli = new CLI();
+    cliController = new CLIController(cli);
+  }
+  public static void start(){
+    cliController.handleRequest();
+  }
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    init();
+
+    while(true){
+      start();
+    }
   }
 }
