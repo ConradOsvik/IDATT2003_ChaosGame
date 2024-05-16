767c7c74db8bf79f54e057d464b1100dce3dd66a
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-05-16 02:56:25 +0200

    feat: add abstract view for gui

--- src/main/java/edu/ntnu/stud/views/View.java
+++ src/main/java/edu/ntnu/stud/views/View.java
@@ -1,11 +1,37 @@
 package edu.ntnu.stud.views;
 
-import javafx.scene.Parent;
+import edu.ntnu.stud.enums.Event;
+import edu.ntnu.stud.utils.Observable;
+import edu.ntnu.stud.utils.Observer;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 
-public class View extends Scene {
+public abstract class View extends Scene implements Observable {
+  protected final List<Observer> observers = new ArrayList<>();
+  protected final VBox root = (VBox) getRoot();
 
-  public View(Parent parent) {
-    super(parent);
+  public View() {
+    super(new VBox());
+  }
+
+  public View(int width, int height) {
+    super(new VBox(), width, height);
+  }
+
+  @Override
+  public void addObserver(Observer observer) {
+    observers.add(observer);
+  }
+
+  @Override
+  public void removeObserver(Observer observer) {
+    observers.remove(observer);
+  }
+
+  @Override
+  public void notifyObservers(Event event) {
+    observers.forEach(observer -> observer.update(event));
   }
 }
