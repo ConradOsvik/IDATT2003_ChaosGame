2d4d33aa65d8ccdea1b7ed12c3bb26569928cb74
ConradOsvik <55027923+ConradOsvik@users.noreply.github.com>
2024-05-16 22:52:41 +0000

    Deploying to javadoc from @ ConradOsvik/IDATT2003_ChaosGame@ea9db2512f6c5d5e9015d05929111ad8a8c27117 🚀

---
+++ javadoc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"edu.ntnu.stud"}];updateSearchResults();
---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"edu.ntnu.stud","l":"edu.ntnu.stud"}];updateSearchResults();
