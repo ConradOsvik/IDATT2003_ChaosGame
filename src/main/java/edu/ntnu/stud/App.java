4aa4a24ee2b6f53c35c5f2f7d07560a7144a895c
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-05-20 02:02:01 +0200

    docs: javadoc App

--- src/main/java/edu/ntnu/stud/App.java
+++ src/main/java/edu/ntnu/stud/App.java
@@ -7,23 +7,47 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * The main class of the application.
+ *
+ * <p>The application can take in arguments. By passing in the argument <b>terminal</b>, it will
+ * launch the application in terminal mode</p>
+ */
 public class App extends Application {
+
   private static CLIController cliController;
 
+  /**
+   * Launches the JavaFX application.
+   */
   @Override
   public void start(Stage stage) throws IOException {
     RootController rootController = new RootController(this, stage);
   }
 
+  /**
+   * Initializes the terminal view and controller
+   */
   public void initTerminal() {
     CLI cli = new CLI();
     cliController = new CLIController(cli);
   }
 
+  /**
+   * Starts the terminal by calling the handleRequest method of cliController
+   *
+   * @see CLIController#handleRequest()
+   */
   public void startTerminal() {
     cliController.handleRequest();
   }
 
+  /**
+   * Launches the terminal application
+   *
+   * <p>The application runs in a loop until you quit, making it possible to perform multiple
+   * actions</p>
+   */
   public static void launchTerminal() {
     App app = new App();
     app.initTerminal();
@@ -33,6 +57,14 @@ public class App extends Application {
     }
   }
 
+  /**
+   * The main method of the application
+   *
+   * <p>It launches the application in terminal mode if the argument <b>terminal</b> is passed
+   * in</p>
+   *
+   * @param args the arguments passed in. <b>terminal</b> to launch in terminal mode
+   */
   public static void main(String[] args) {
     if (args.length > 0 && args[0].equals("terminal")) {
       launchTerminal();
