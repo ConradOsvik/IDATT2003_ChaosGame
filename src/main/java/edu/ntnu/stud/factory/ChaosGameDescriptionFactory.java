65a4075d3a68a8d6c2d23fc9123560daa5657d18
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-05-21 13:38:34 +0200

    feat: create basic description factory

--- src/main/java/edu/ntnu/stud/factory/ChaosGameDescriptionFactory.java
+++ src/main/java/edu/ntnu/stud/factory/ChaosGameDescriptionFactory.java
@@ -1,30 +1,64 @@
 package edu.ntnu.stud.factory;
 
+import edu.ntnu.stud.models.AffineTransform2D;
 import edu.ntnu.stud.models.ChaosGameDescription;
+import edu.ntnu.stud.models.Matrix2x2;
+import edu.ntnu.stud.models.Transform2D;
+import edu.ntnu.stud.models.Vector2D;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ChaosGameDescriptionFactory implements ChaosGameDescriptionFactoryInterface {
-  
-  /**
-   * @return
-   */
-  @Override
-  public ChaosGameDescription createJuliaDescription() {
-    return null;
+public class ChaosGameDescriptionFactory {
+  public static ChaosGameDescription createSierpinskiDescription() {
+    Vector2D minCoords = new Vector2D(0, 0);
+    Vector2D maxCoords = new Vector2D(1, 1);
+    List<Transform2D> transforms = new ArrayList<>();
+
+    Matrix2x2 matrix1 = new Matrix2x2(0.5, 0, 0, 0.5);
+    Vector2D vector1 = new Vector2D(0, 0);
+    AffineTransform2D transform1 = new AffineTransform2D(matrix1, vector1);
+
+    Matrix2x2 matrix2 = new Matrix2x2(0.5, 0, 0, 0.5);
+    Vector2D vector2 = new Vector2D(0.5, 0);
+    AffineTransform2D transform2 = new AffineTransform2D(matrix2, vector2);
+
+    Matrix2x2 matrix3 = new Matrix2x2(0.5, 0, 0, 0.5);
+    Vector2D vector3 = new Vector2D(0.25, 0.5);
+    AffineTransform2D transform3 = new AffineTransform2D(matrix3, vector3);
+
+    transforms.add(transform1);
+    transforms.add(transform2);
+    transforms.add(transform3);
+
+    return new ChaosGameDescription(transforms, minCoords, maxCoords);
   }
-  
-  /**
-   * @return
-   */
-  @Override
-  public ChaosGameDescription createBarnsleyDescription() {
-    return null;
-  }
-  
-  /**
-   * @return
-   */
-  @Override
-  public ChaosGameDescription createSierpinskiDescription() {
-    return null;
+
+  public static ChaosGameDescription createBarnsleyDescription(){
+    Vector2D minCoords = new Vector2D(-2.65, 0);
+    Vector2D maxCoords = new Vector2D(2.65, 10);
+    List<Transform2D> transforms = new ArrayList<>();
+
+    Matrix2x2 matrix1 = new Matrix2x2(0, 0, 0, 0.16);
+    Vector2D vector1 = new Vector2D(0, 0);
+    AffineTransform2D transform1 = new AffineTransform2D(matrix1, vector1);
+
+    Matrix2x2 matrix2 = new Matrix2x2(0.85, 0.04, -0.04, 0.85);
+    Vector2D vector2 = new Vector2D(0, 1.6);
+    AffineTransform2D transform2 = new AffineTransform2D(matrix2, vector2);
+
+    Matrix2x2 matrix3 = new Matrix2x2(0.2, -0.26, 0.23, 0.22);
+    Vector2D vector3 = new Vector2D(0, 1.6);
+    AffineTransform2D transform3 = new AffineTransform2D(matrix3, vector3);
+
+    Matrix2x2 matrix4 = new Matrix2x2(-0.15, 0.28, 0.26, 0.24);
+    Vector2D vector4 = new Vector2D(0, 0.44);
+    AffineTransform2D transform4 = new AffineTransform2D(matrix4, vector4);
+
+    transforms.add(transform1);
+    transforms.add(transform2);
+    transforms.add(transform3);
+    transforms.add(transform4);
+
+    return new ChaosGameDescription(transforms, minCoords, maxCoords);
   }
 }
