2c3acdc9eb00bfb25487df4cff7d3c70f5a988bb
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-05-21 13:38:55 +0200

    ref: remove unnecessary interface

--- src/main/java/edu/ntnu/stud/factory/ChaosGameDescriptionFactoryInterface.java
+++
@@ -1,9 +0,0 @@
-package edu.ntnu.stud.factory;
-
-import edu.ntnu.stud.models.ChaosGameDescription;
-
-public interface ChaosGameDescriptionFactoryInterface {
-    ChaosGameDescription createJuliaDescription();
-    ChaosGameDescription createBarnsleyDescription();
-    ChaosGameDescription createSierpinskiDescription();
-}
